4f111fb4174ef4187c63508f4fae80b872c157e7
Artsiom Balakir <abalak8@illinois.edu>
2019-04-24 09:36:03 -0500

    dump from work station

--- dataloader/base.py
+++ dataloader/base.py
@@ -13,28 +13,42 @@ class BaseDataset:
         np.random.shuffle(idx)
 
         valid_size = np.int(N * self.valid_split)
-        test_size = np.int(N * self.test_split)
+        # test_size = np.int(N * self.test_split)
+        test_size = 0
         train_size = N - valid_size - test_size
 
         train_idx = idx[:train_size]
         valid_idx = idx[train_size: train_size + valid_size]
-        test_idx = idx[-test_size:]
+        train_idx.sort()
+        valid_idx.sort()
+        # test_idx = idx[-test_size:]
 
-        _X = np.empty(X.shape, dtype=np.float64, order='C')
-        _Y = np.empty(Y.shape, dtype=np.float64, order='C')
+        # _X = np.empty(X.shape, dtype=np.float64, order='C')
+        # _Y = np.empty(Y.shape, dtype=np.float64, order='C')
 
-        X.read_direct(_X)
-        Y.read_direct(_Y)
+        x_train = np.empty(
+            (train_size, *X.shape[1:]), dtype=np.float32, order='C')
+        y_train = np.empty(
+            (train_size, *Y.shape[1:]), dtype=np.float32, order='C')
+        x_valid = np.empty(
+            (valid_size, *X.shape[1:]), dtype=np.float32, order='C')
+        y_valid = np.empty(
+            (valid_size, *Y.shape[1:]), dtype=np.float32, order='C')
 
-        x_train = _X[train_idx]
-        y_train = _Y[train_idx]
+        X.read_direct(x_train, list(train_idx))
+        Y.read_direct(y_train, list(train_idx))
+        X.read_direct(x_valid, list(valid_idx))
+        Y.read_direct(y_valid, list(valid_idx))
 
-        x_test = _X[test_idx]
-        y_test = _Y[test_idx]
+        # x_train = _X[train_idx]
+        # y_train = _Y[train_idx]
 
-        x_valid = _X[valid_idx]
-        y_valid = _Y[valid_idx]
+        # x_test = _X[test_idx]
+        # y_test = _Y[test_idx]
+
+        # x_valid = _X[valid_idx]
+        # y_valid = _Y[valid_idx]
 
         self.train = (x_train, y_train)
-        self.test = (x_test, y_test)
+        # self.test = (x_test, y_test)
         self.valid = (x_valid, y_valid)
