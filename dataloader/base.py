3c6a22f3efb7cc0c62d34baf7c79d891893d7421
Artsiom Balakir <abalak8@illinois.edu>
2019-10-11 17:20:36 -0500

    added support to run without test split

--- dataloader/base.py
+++ dataloader/base.py
@@ -17,11 +17,9 @@ class BaseDataset:
 
         train_idx = idx[:train_size]
         valid_idx = idx[train_size: train_size + valid_size]
-        test_idx = idx[-test_size:]
 
         train_idx.sort()
         valid_idx.sort()
-        test_idx.sort()
 
         x_train = np.empty(
             (train_size, *X.shape[1:]), dtype=np.float32, order='C')
@@ -32,38 +30,47 @@ class BaseDataset:
         y_valid = np.empty(
             (valid_size, *Y.shape[1:]), dtype=np.float32, order='C')
 
-        x_test = np.empty(
-            (test_size, *X.shape[1:]), dtype=np.float32, order='C')
-        y_test = np.empty(
-            (test_size, *Y.shape[1:]), dtype=np.float32, order='C')
 
         X.read_direct(x_train, list(train_idx))
         Y.read_direct(y_train, list(train_idx))
         X.read_direct(x_valid, list(valid_idx))
         Y.read_direct(y_valid, list(valid_idx))
-        X.read_direct(x_test, list(test_idx))
-        Y.read_direct(y_test, list(test_idx))
+
 
         class __Object(object): pass
 
         self.train = __Object()
-        self.test = __Object()
         self.valid = __Object()
 
         self.train.xy = (x_train, y_train)
         self.valid.xy = (x_valid, y_valid)
-        self.test.xy = (x_test, y_test)
+
+        if test_size > 0:
+            self.test = __Object()
+            test_idx = idx[-test_size:]
+            test_idx.sort()
+            x_test = np.empty(
+                (test_size, *X.shape[1:]), dtype=np.float32, order='C')
+            y_test = np.empty(
+                (test_size, *Y.shape[1:]), dtype=np.float32, order='C')
+            X.read_direct(x_test, list(test_idx))
+            Y.read_direct(y_test, list(test_idx))
+            self.test.xy = (x_test, y_test)
+        else:
+            self.test = None
 
         if names != None:
             dtype = names.dtype
             names_train = np.empty((train_size), dtype=dtype)
             names_valid = np.empty((valid_size), dtype=dtype)
-            names_test = np.empty((test_size), dtype=dtype)
 
             names.read_direct(names_train, list(train_idx))
             names.read_direct(names_valid, list(valid_idx))
-            names.read_direct(names_test, list(test_idx))
 
             self.train.names = names_train
             self.valid.names = names_valid
-            self.test.names = names_test
+
+            if test_size > 0:
+                names_test = np.empty((test_size), dtype=dtype)
+                names.read_direct(names_test, list(test_idx))
+                self.test.names = names_test
